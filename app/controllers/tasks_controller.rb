cb18e5a27b3344f16ff7332ddd496dcafa1dc17e
Ligia <mktligia@gmail.com>
2021-08-05 13:13:46 -0400

    segunda parte del ejercicio

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,37 @@
 class TasksController < ApplicationController
-  def new
+  def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])  
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+    redirect_to tasks_path(@task)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id]) 
+  end
+
+  def update
+    
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #Crear
+  get "tasks/new", to:"tasks#new", as: :new
+  post "tasks", to: "tasks#create"
+  #Read
   get "tasks", to: "tasks#index"
-  get "task/:id", to: "tasks#show", as: :task
+  get "tasks/:id", to: "tasks#show", as: :task
+  #Update
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "s/:id", to: "restaurants#update"
 end
+#verb path, to: "controller#method"
+
+
